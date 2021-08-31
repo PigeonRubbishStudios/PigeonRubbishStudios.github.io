146f0e60fc5743d1dd6de2f24a5d470dd0c2f915
Kyle Bolinger <pigeonrubbish@gmail.com>
2021-08-31 12:08:26 -0500

    Updated Paths

--- sites/message/js/main.js
+++ sites/message/js/main.js
@@ -8,33 +8,31 @@ let dayName = d.getDay();
 
 if (dayName == 0)
 {
-    document.body.style.background = "url('../images/1.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/1.jpg')";
 }
 else if (dayName == 1)
 {
-    document.body.style.background = "url('../images/2.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/2.jpg')";
 }
 else if (dayName == 2)
 {
-    document.body.style.background = "url('../images/3.jpg')";
-    document.body.style.backgroundSize = "cover";
-
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/3.jpg')";
 }
 else if (dayName == 3)
 {
-    document.body.style.background = "url('../images/4.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/4.jpg')";
 }
 else if (dayName == 4)
 {
-    document.body.style.background = "url('../images/5.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/5.jpg')";
 }
 else if (dayName == 5)
 {
-    document.body.style.background = "url('../images/6.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/6.jpg')";
 }
 else if (dayName == 6)
 {
-    document.body.style.background = "url('../images/7.jpg')";
+    document.body.style.background = "url('https://pigeonrubbishstudios.github.io/sites/message/images/7.jpg')";
 }
 
-let pos = document.getElementById("first-line");
+document.body.style.backgroundSize = "cover";
