40f0d5dfe8b22b92647c18304c6dc342e5946a5e
Kyle Bolinger <pigeonrubbish@gmail.com>
2021-08-31 12:36:56 -0500

    Edited Remi Position

--- sites/message/js/animation.js
+++ sites/message/js/animation.js
@@ -8,4 +8,4 @@ tl.to('#third-line', {opacity: 1, transform: "translateY(-10%)", duration: 1.5, 
 
 tl.to('#fourth-line', {opacity: 1, transform: "translateY(-10%)", duration: 1.5, ease: "power2.inOut"});
 
-tl.to('#fifth-line', {opacity: 1, transform: "translateY(-10%)", duration: 1.5, ease: "power2.inOut"});
+tl.to('#fifth-line', {opacity: 1, transform: "translate(-50%, -10%)", duration: 1.5, ease: "power2.inOut"});
