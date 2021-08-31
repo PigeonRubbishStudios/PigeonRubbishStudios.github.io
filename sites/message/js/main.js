5412ec4550e57ea65b45818d641c066f07db6a7a
Kyle Bolinger <pigeonrubbish@gmail.com>
2021-08-31 11:37:51 -0500

    Updated Paths

--- sites/message/js/main.js
+++ sites/message/js/main.js
@@ -8,33 +8,33 @@ let dayName = d.getDay();
 
 if (dayName == 0)
 {
-    document.body.style.background = "url('images/1.jpg')";
+    document.body.style.background = "url('../images/1.jpg')";
 }
 else if (dayName == 1)
 {
-    document.body.style.background = "url('/images/2.jpg')";
+    document.body.style.background = "url('../images/2.jpg')";
 }
 else if (dayName == 2)
 {
-    document.body.style.background = "url('/images/3.jpg')";
+    document.body.style.background = "url('../images/3.jpg')";
     document.body.style.backgroundSize = "cover";
 
 }
 else if (dayName == 3)
 {
-    document.body.style.background = "url('/images/4.jpg')";
+    document.body.style.background = "url('../images/4.jpg')";
 }
 else if (dayName == 4)
 {
-    document.body.style.background = "url('/images/5.jpg')";
+    document.body.style.background = "url('../images/5.jpg')";
 }
 else if (dayName == 5)
 {
-    document.body.style.background = "url('/images/6.jpg')";
+    document.body.style.background = "url('../images/6.jpg')";
 }
 else if (dayName == 6)
 {
-    document.body.style.background = "url('/images/7.jpg')";
+    document.body.style.background = "url('../images/7.jpg')";
 }
 
 let pos = document.getElementById("first-line");
